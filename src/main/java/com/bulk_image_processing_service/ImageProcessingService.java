7459705aea3ac55344b8d82b447f6fa1eba59cfd
darshan-trivedi-10 <trivedidarshan30@gmail.com>
2025-03-01 21:57:31 +0530

    adding new spring boot project

---
+++ src/main/java/com/bulk_image_processing_service/ImageProcessingService.java
@@ -0,0 +1,13 @@
+package com.bulk_image_processing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessingService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageProcessingService.class, args);
+	}
+
+}
---
+++ src/test/java/com/bulk_image_processing_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulk_image_processing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
